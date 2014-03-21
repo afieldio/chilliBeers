e2be78fb26611f7b33fcee6ee6f8f2a449a9d9f5
adamField <a.field738@gmail.com>
2014-03-21 14:06:53 +0000

    cleaned

--- application/data/items.php
+++ application/data/items.php
@@ -28,11 +28,11 @@ function getItemById($link, $id){
 
 function selectItemByCategory($link, $category){
 
-	select 
-	items.id, items.item, items_have_category.items_id, items_have_category.category_id, category.category from items
-	inner join items_have_category
-	on items.id = items_have_category.items_id
-	inner join category
-	on `category`.id =items_have_category.category_id
-	where category.category like 'drink'
+	// select 
+	// items.id, items.item, items_have_category.items_id, items_have_category.category_id, category.category from items
+	// inner join items_have_category
+	// on items.id = items_have_category.items_id
+	// inner join category
+	// on `category`.id =items_have_category.category_id
+	// where category.category like 'drink'
 }
